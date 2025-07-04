c901512a04b78afa6bbb43caaa08e0eeeb02d456
김솔지 <so1gging@gmail.com>
2025-07-04 20:46:44 +0900

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    // 필요에 따라 규칙 추가
+  },
+};
