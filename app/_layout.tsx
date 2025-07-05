78d1625dd4518ebfde770c7f934d5ce63333d108
김솔지 <so1gging@gmail.com>
2025-07-05 16:28:49 +0900

    feat: route group 기능 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
 import { Stack } from 'expo-router';
 
-export default function Layout() {
+export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(map)" />
     </Stack>
   );
 }
