ae48bb89009792b2810db8591599ba1d7400c5ad
김솔지 <so1gging@gmail.com>
2025-07-05 15:49:07 +0900

    chore: 로그 제거

--- components/MapView/MapView.tsx
+++ components/MapView/MapView.tsx
@@ -16,9 +16,7 @@ var mapOptions = {
     center: new naver.maps.LatLng(37.3595704, 127.105399),
     zoom: 10
 };
-    var map = new naver.maps.Map('map', mapOptions);
-    // window.ReactNativeWebView.postMessage();
-    
+    var map = new naver.maps.Map('map', mapOptions); 
 </script>
 </body>
 </html>
