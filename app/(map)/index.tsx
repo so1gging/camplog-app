1e9089dffc049526dbfd8a0c5a18a13188bae193
김솔지 <so1gging@gmail.com>
2025-07-05 21:15:11 +0900

    feat: 캠핑 데이터 테스트

--- app/(map)/index.tsx
+++ app/(map)/index.tsx
@@ -1,7 +1,24 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import { NaverMapView } from '@mj-studio/react-native-naver-map';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `https://apis.data.go.kr/B551011/GoCamping/basedList?MobileOS=IOS&MobileApp=Camplog&serviceKey=${process.env.EXPO_PUBLIC_SERVICE_KEY}&_type=json`,
+        );
+        console.log(res.data.response.body.items.item);
+      } catch (e) {
+        console.error('Fetch error:', e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <NaverMapView style={{ flex: 1 }} />
