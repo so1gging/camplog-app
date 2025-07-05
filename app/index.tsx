81bfe47760459d72d2fe04597ea085d0e04e1a92
김솔지 <so1gging@gmail.com>
2025-07-05 15:59:25 +0900

    style: 지도가 꽉 차도록

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
 import MapView from '@/components/MapView/MapView';
+import { SafeAreaView } from 'react-native';
 
 export default function Home() {
-  return <MapView />;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <MapView />
+    </SafeAreaView>
+  );
 }
