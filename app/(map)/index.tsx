3a116b8420aee82483ff40ebc610d8f14cf52f08
김솔지 <so1gging@gmail.com>
2025-07-06 11:23:11 +0900

    feat: 대한민국 지도가 한눈에 보이는 시점으로 region 변경

--- app/(map)/index.tsx
+++ app/(map)/index.tsx
@@ -3,6 +3,13 @@ import { NaverMapView } from '@mj-studio/react-native-naver-map';
 import { useEffect } from 'react';
 import axios from 'axios';
 
+const INIT_REGION = {
+  latitude: 33.99611583543648,
+  longitude: 125.83090197375878,
+  latitudeDelta: 5.683935821120691,
+  longitudeDelta: 4.11987292965938,
+};
+
 export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
@@ -10,7 +17,7 @@ export default function Home() {
         const res = await axios.get(
           `${process.env.EXPO_PUBLIC_DATA_API_URL}/basedList?MobileOS=IOS&MobileApp=Camplog&serviceKey=${process.env.EXPO_PUBLIC_SERVICE_KEY}&_type=json`,
         );
-        console.log(res.data.response.body.items.item);
+        // console.log(res.data.response.body.items.item);
       } catch (e) {
         console.error('Fetch error:', e);
       }
@@ -21,7 +28,7 @@ export default function Home() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <NaverMapView style={{ flex: 1 }} />
+      <NaverMapView style={{ flex: 1 }} initialRegion={INIT_REGION} />
     </SafeAreaView>
   );
 }
